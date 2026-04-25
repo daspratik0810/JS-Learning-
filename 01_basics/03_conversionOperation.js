df6198f14ba09458353c21a34e8419d7f47bb485
Pratik Das <daspratik0810@gmail.com>
2026-04-25 19:30:24 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = 33
+console.log(typeof score);  //number
+
+let changeToString = String(score)  //used to chane the datatype to string
+
+console.log(typeof changeToString);  // string
+
+let score2 = "33abc"  
+console.log(typeof score2);   //string
+
+let changeToNumber = Number(score2)  //changed to number
+console.log(typeof changeToNumber);  // number
+console.log(changeToNumber);  // actually its not a number, but is NaN
+ 

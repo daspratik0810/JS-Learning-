6cadd43e8ae238cfc1f966936aa77210c48b3502
Pratik Das <daspratik0810@gmail.com>
2026-04-25 01:25:20 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+//code readability should be the most priority
+//use https://tc39.es/ecma262/  which is the official documentation of js
+//but it is bit difficult to understand so we can easily understand JS with "mdn docs for js" given by morjilla organization
+
+let name = "Pratik"   //string
+let age = 24   // number 
+let isLoggedIn = false  //boolean
+
+//number - 2 to power 53
+//bigInt 
+//string - ""
+//boolean - true or false
+// null - standalone value (its a type is object)
+//undefined - when variable is defined but the value is not assigned  (its a type of undefined )
+//symbol - unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);  //(its a type is object)
+console.log(typeof undefined);  //(its a type of undefined )
+

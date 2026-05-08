41ff0cdb14e64f224eade7d2064939d371f6fed2
Pratik Das <daspratik0810@gmail.com>
2026-05-08 03:39:55 +0000

    BMI-final

--- Project/03_BMI-Calculator/code.js
+++ Project/03_BMI-Calculator/code.js
@@ -1,3 +1,6 @@
+const bd = document.querySelector("body")
+bd.style.backgroundColor = "#ab5c5cb1"
+
 const form= document.querySelector("form")
 
 form.addEventListener("submit", function(event){
@@ -14,20 +17,20 @@ form.addEventListener("submit", function(event){
     }else if(weight === "" || weight < 0 || isNaN(weight)) {
     results.innerHTML = `Please give a valid weight ${weight}`
     }else {
-    const bmi = ( (weight / (height * height ) )*10000).toFixed(2)
-    results.innerHTML =  `${bmi}`
+        const bmi = ( (weight / (height * height ) )*10000).toFixed(2)
+        console.log(bmi);
+        results.innerHTML =  `${bmi}`
+        
+        if(bmi<=18.6){
+            console.log("Under Weight");
+            description.innerHTML = "Unfortunately !! You Are Under Weight"
+        }else if(bmi>18.6 && bmi<=24.9){
+            console.log("Normal Range");
+            description.innerHTML = "Congratulations !! You are in Normal Range"
+
+        }else{
+            console.log("Over Weight");
+            description.innerHTML = "Unfortunately !! You Are Over Weight"
+        }
     }
-
-     if(bmi<=18.6){
-        console.log("Under Weight");
-        description.innerHTML = "Unfortunately !! You Are Under Weight"
-    }else if(bmi>18.6 && bmi<=24.9){
-        console.log("Normal Range");
-        description.innerHTML = "Congratulations !! You are in Normal Range"
-
-    }else{
-        console.log("Over Weight");
-        description.innerHTML = "Unfortunately !! You Are Over Weight"
-    }
-
 })

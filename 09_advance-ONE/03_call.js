7567f91a2c755484eb3ec186d044d1db441f60fc
Pratik Das <daspratik0810@gmail.com>
2026-05-20 22:09:29 +0000

    call

--- 09_advance-ONE/03_call.js
+++ 09_advance-ONE/03_call.js
@@ -6,10 +6,11 @@ function SetUsername(username){
 
 function createUser(username, email, password){
    //this.username = username //this will also work, but let us play more
-    SetUsername.call(this.username)
+   //SetUsername (username) //it wont work, because it does not call SetUsername, instead only gives the refrence and it is a limitation of JS
+    SetUsername.call(this, username) //It will work now, by using call() method.It will call the SetUsername function
     this.email = email
     this.password = password
 }
 
-const chai = new createUser("chaii", "chai@fb.com", "123")
+const chai = new createUser("chai", "chai@fb.com", "123")
 console.log(chai);

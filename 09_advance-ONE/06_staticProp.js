f54a973d19f4052c5670da2bce8794a0dd0a56bf
Pratik Das <daspratik0810@gmail.com>
2026-05-21 14:53:11 +0000

    sttais properties in class

---
+++ 09_advance-ONE/06_staticProp.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME IS: ${this.username}`);
+    }
+
+    static createID(){      //here static keyword restricts parent and also child to imherit the method createID()
+        return `123`
+    }
+}
+
+const hitesh = new User ("hitesh")
+
+hitesh.logMe()
+console.log(hitesh.createID())  // because of static keyword, it failed to take the createMe() function/mentod 
